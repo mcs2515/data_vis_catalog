48a596ae5c50dba321dc5a102a42bdbfbc6f7ebe
mcs2515 <mcs2515@rit.edu>
2018-11-07 19:57:34 -0500

    finished barchart. added styling. starting stackedchart

--- js/barchart.js
+++ js/barchart.js
@@ -1,11 +1,117 @@
 function rowConverter(row) {
-  return {
+	return {
+		class: row.class,
+		boxtops: parseInt(row.boxtops),
 	}
-}
+};
+
+let dataset;
 
 function makeChart(dataset) {
+
+	let w = 900;
+	let h = 450;
+	let marginT = 20;
+	let marginL = 20;
+	let marginR = 20;
+	let marginB = 50;
+
+	let tooltip = d3.select("body").append("div").attr('id', 'tooltip').style("opacity", 0);
+
+	//bar width = (width of chart - margins ) / length of dataset  - padding
+	let barwidth = (w - (marginL + marginR)) / (dataset.length) - 15;
+
+	//set overall chart size
+	let chart = d3.select('#barchart')
+		.attr('width', w)
+		.attr('height', h);
+
+	let xScale = d3.scaleLinear()
+		.domain([0, 11])
+		.range([marginL, w - marginR]);
+
+	let yScale =
+		d3.scaleLinear()
+		.domain([0, d3.max(dataset, d => d.boxtops) + 10])
+		.range([h - marginB, marginT]);
+
+	let xAxis = d3.axisBottom(xScale);
+	let yAxis = d3.axisLeft(yScale);
+
+	chart.selectAll('rect')
+		.data(dataset)
+		.enter()
+		.append('rect')
+		.attr('x', d => xScale(d.class))
+		.attr('y', d => yScale(d.boxtops))
+		.attr('width', barwidth)
+		.attr('height', d => h - yScale(d.boxtops) - marginB)
+		.attr('transform', `translate(5,0)`)
+		.style('fill', '#7293cb')
+		.on('mousemove', function (d) {
+
+			d3.select(this)
+				.transition("fill")
+				.duration(250)
+				.style('fill', '#2162d1')
+				.style('cursor', 'pointer');
+
+			tooltip
+				.style('left', (d3.event.pageX) - 50 + "px")
+				.style('top', (d3.event.pageY) - 40 + "px")
+				.text("Collected: " + d.boxtops)
+				.transition("tooltip")
+				.duration(200)
+				.style("opacity", .8);
+		})
+		.on('mouseout', function (d) {
+			d3.select(this)
+				.transition("fill")
+				.duration(250)
+				.style('fill', '#7293cb');
+
+			tooltip
+				.transition("tooltip")
+				.duration(500)
+				.style("opacity", 0);
+		});
+
+	xAxis.tickFormat(function (d, i) {
+		if (i > 0 && i < 11) return "Class " + d
+	});
+
+	chart.append('g')
+		.attr('transform', `translate(40, ${h-marginB})`)
+		.call(xAxis);
+
+	chart.append('g')
+		.attr('transform', `translate(60, 0)`)
+		.call(yAxis);
+
+	// LABELS
+	//y-axis
+	chart.append("text")
+		.attr("class", "labels")
+		.attr("transform", "rotate(-90)")
+		.attr("x", -(h - marginB) / 2)
+		.attr("y", 20)
+		.style("text-anchor", "middle")
+		.text("Amount Collected");
+
+	//x-axis
+	chart.append("text")
+		.attr("class", "labels")
+		.attr("x", h)
+		.attr("y", (w / 2))
+		.style("text-anchor", "middle")
+		.text("Classes");
 }
 
 
 window.onload = function () {
+	d3.csv('../datasets/boxtops.csv', rowConverter)
+		.then((d) => {
+			dataset = d;
+			makeChart(dataset)
+		});
 }
--- js/stackedbar.js
+++ js/stackedbar.js
@@ -1,11 +1,42 @@
 function rowConverter(row) {
-  return {
+	return {
+		country: row.country,
+		public: parseFloat(row.public),
+		private: parseFloat(row.private),
 	}
 }
 
+let dataset;
+
 function makeChart(dataset) {
+	let w = 900;
+	let h = 450;
+	let marginT = 20;
+	let marginL = 20;
+	let marginR = 20;
+	let marginB = 50;
+	console.log(dataset.length);
+	//bar width = (width of chart - margins ) / length of dataset  - padding
+	let barwidth = (w - (marginL + marginR)) / (dataset.length) - 15;
+	
+	let chart = d3.select('#stackedbar')
+		.attr('width', w)
+		.attr('height', h);
+	
+	let xScale = d3.scaleOrdinal()
+		.domain(dataset.map((d) => dataset.country))
+		.range(marginL, w-marginR);
+	
+	let yScale = d3.scalelinear()
+		.domain([0, d3.max(dataset, d => d.public)]+1)
+		.domain([h-marginB, marginT])
 }
 
 
 window.onload = function () {
+	d3.csv('../datasets/healthcare.csv', rowConverter)
+		.then((d) => {
+			dataset = d;
+			makeChart(dataset);
+		});
 }
